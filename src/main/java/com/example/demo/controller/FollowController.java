771124b8ba95c16054760b55763bacf165e4b14b
nilepa <nilepa@naver.com>
2021-09-17 10:25:50 +0900

    ok

--- src/main/java/com/example/demo/controller/FollowController.java
+++ src/main/java/com/example/demo/controller/FollowController.java
@@ -42,39 +42,6 @@ public class FollowController {
 	public void setUsersdao(UsersDao usersdao) {
 		this.usersdao = usersdao;
 	}
-	/*
-	@RequestMapping("/mypage/followList.do")
-	public ModelAndView followList(@RequestParam(value="pageNUM",defaultValue = "1") int pageNUM, Model model,HttpSession session,String users_id) {
-		System.out.println("users_id:"+users_id);
-		
-		System.out.println("controller 팔로우 pageNUM:"+pageNUM);
-		FollowDao.totalRecord = fdao.getTotalRecord(users_id);
-		FollowDao.totalPage = (int)Math.ceil((double)FollowDao.totalRecord/FollowDao.pageSIZE);
-		
-		int start = (pageNUM-1)*FollowDao.pageSIZE+1;
-		int end = start+FollowDao.pageSIZE-1;
-		if(end > FollowDao.totalRecord ) {
-			end = FollowDao.totalRecord;
-		}
-		
-		System.out.println("f start:"+start);
-		System.out.println("f end:"+ end);
-		HashMap map = new HashMap();
-		map.put("start", start);
-		map.put("end", end);
-		map.put("users_id", users_id);
-		System.out.println("팔로우controller users_id"+users_id);
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("list",fdao.listFollow(map));
-		
-		List<UsersVO> list = fdao.listFollow(map);
-		System.out.println("팔로우목록:"+list);
-		model.addAttribute("flist",list);
-		model.addAttribute("totalPage",FollowDao.totalPage);
-		return mav;
-	}*/
-	
-	
 	
 	@ResponseBody
 	@RequestMapping(value = "/insertFollow.do")
